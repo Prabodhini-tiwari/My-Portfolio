bf62da42f11548334ee53c7ff09d0bb286ab4f0a
Prabodhini Tiwari <prabhutiwari143@gmail.com>
2024-08-27 16:52:46 +0300

    Add smooth scrolling for navigation links

--- src/Components/myPortfolio/Navbar/MobileNav/MobileNav.jsx
+++ src/Components/myPortfolio/Navbar/MobileNav/MobileNav.jsx
@@ -4,15 +4,32 @@ import './MobileNav.css';
 const MobileNav = ({ isOpen, toggleMenu }) => {
     return (
         <div className={`mobile-menu ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
-            <div className="mobile-menu-container">
+            <div className="mobile-menu-container" onClick={(e) => e.stopPropagation()}>
                 <div className='portfolio'>Prabodhini's Portfolio</div>
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#About">About</a></li>
-                    <li><a href="#Skills">Skills</a></li>
-                    <li><a href="#Experience">Experience</a></li>
-                    <li><a href="#Projects">Projects</a></li>
-                    <li><a href="#Contacts">Contacts</a></li>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+
+                    <li>
+                        <a href="#AboutAndSkills">About</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+
+                    <li>
+                        <a href="#experience">Experience</a>
+                    </li>
+
+                    <li>
+                        <a href="#education">Education</a>
+                    </li>
+
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
                 </ul>
             </div>
         </div>
